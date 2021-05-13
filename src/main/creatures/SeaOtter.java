c48916a14e8c5d75cf3b4915000040014a24f02f
atgil <Gilleylenat21@brightonk12.com>
2021-05-13 08:44:57 -0400

    Added sea otter class

---
+++ src/main/creatures/SeaOtter.java
@@ -0,0 +1,16 @@
+package creatures;
+
+
+    public class SeaOtter extends AquaticMammal {
+
+        public void method1() {
+            System.out.println("Sea otters have hydrophobic fur");
+        }
+
+
+        public String toString() {
+            return "Sea Otter";
+        }
+    }
+
+
